39dee9e27f87d68ff6e107a70247619dbcab04b7
vagrant <vagrant@homestead>
2020-11-18 16:52:00 +0000

    限制头像分辨率

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -10,7 +10,7 @@ use  Illuminate\Support\Str;
 class ImageUploadHandler
 {
     // 只允许以下后缀名的图片文件上传
-    protected $allowed_ext = ["png", "jpg", "gif", 'jpeg'];
+    protected $allowed_ext = ["png", "jpg", "gif", 'jpeg','bmp'];
 
     public function save($file, $folder, $file_prefix)
     {
--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -25,7 +25,8 @@ class UserRequest extends FormRequest
     {
         return [
             'name'=>'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/',
-            'introduction'=>'max:80'
+            'introduction'=>'max:80',
+            'avatar'=>'mimes:jpg,jpeg,png,bmp,gif|dimensions:min_width=208,min_height=208'
         ];
     }
 
@@ -36,6 +37,8 @@ class UserRequest extends FormRequest
           'name.between'=>'用户名介于3到25个字符中间',
           'name.regex'=>'用户名只支持英文、数字、横杠和下划线',
           'introduction.max'=>'个人简介不能超过80个字符长度',
+            'avatar.mimes'=>'图片格式必须为jpg,jpeg,png,bmp,gif',
+            'avatar.dimensions'=>'清晰度不够，请上传208*208以上图片',
         ];
     }
 }
