74464219f9c36f066c8999be91a5166ab11319f9
vagrant <vagrant@homestead>
2020-11-18 17:58:31 +0000

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,7 +9,16 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+
     //
+    /**
+     * UsersController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth',['except'=>['show']]);
+    }
+
     public function show(User $user)
     {
         return view('users.show',compact('user'));
@@ -17,11 +26,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
     }
 
     public function update(UserRequest $request,ImageUploadHandler $uploader,User $user)
     {
+        $this->authorize('update',$user);
         $data=$request->except('email');
         if($request->avatar){
             $result=$uploader->save($request->avatar,'avatars',$user->id,416);
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::guessPolicyNamesUsing(function ($modelClass){
+               return 'App\Policies\\'.$modelClass.'Policy';
+        });
     }
 }
