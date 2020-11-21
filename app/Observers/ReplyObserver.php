052d869a126bcc9eebe215442dcee1c1434e7150
vagrant <vagrant@homestead>
2020-11-21 18:36:14 +0000

    删除评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -16,7 +16,8 @@ class ReplyObserver
         /*\DB::enableQueryLog();
         $reply->topic()->increment("reply_count",1);
         dd(\DB::getQueryLog());*/
-        $reply->topic->reply_count=$reply->topic->replies->count();
+//        $reply->topic->reply_count=$reply->topic->replies->count();
+        $reply->topic->updateReplyCount();
         $reply->topic->save();
         //通知话题作者有新的评论
         $reply->topic->user->notify(new TopicReplied($reply));
@@ -27,8 +28,8 @@ class ReplyObserver
         $reply->content=clean($reply->content,'user_topic_body');
     }
 
-    public function updating(Reply $reply)
+    public function deleted(Reply $reply)
     {
-        //
+        $reply->topic->updateReplyCount();
     }
 }
