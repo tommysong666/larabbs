4cca09a84155a77c541892b606c1de5e17043876
vagrant <vagrant@homestead>
2020-11-23 11:38:19 +0000

    侧边栏链接

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Link;
 use App\Models\Topic;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -10,11 +11,12 @@ use Illuminate\Http\Request;
 class CategoriesController extends Controller
 {
     //
-    public function show(Request $request,Category $category,User $user)
+    public function show(Request $request,Category $category,User $user,Link $link)
     {
         $topics=Topic::where('category_id',$category->id)->withOrder($request->order)->with('user','category')->paginate(20);
         // 活跃用户列表
         $active_users = $user->getActiveUsers();
-        return view('topics.index',compact('topics','category','active_users'));
+        $links=$link->getAllCached();
+        return view('topics.index',compact('topics','category','active_users','links'));
     }
 }
